dcdc1479204564d03feb2504aa25d656ba0ad002
Daniel Gamboa <danielgamboa10@gmail.com>
2020-11-14 14:00:56 -0600

    Create models including database connection configuration, setup_db function, Actors class, Movies class and association table

---
+++ backend/models.py
@@ -0,0 +1,98 @@
+import os
+from sqlalchemy import Column, String, Integer, DateTime, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+
+database_name = "agency"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+
+# Binds a flask application and a SQLAlchemy service
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+#----------------------------------------------------------------------------#
+# Association Table
+#----------------------------------------------------------------------------#
+actor_movies = db.Table('actor_movies',
+    db.Column('actor_id', db.Integer, db.ForeignKey('actors.id'), primary_key=True),
+    db.Column('movie_id', db.Integer, db.ForeignKey('novies.id'), primary_key=True)
+)
+
+
+#----------------------------------------------------------------------------#
+# Actors Model
+#----------------------------------------------------------------------------#
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer)
+    gender = Column(String)
+    movies = db.relationship('Movie', secondary=actor_movies, backref=db.backref('actors', lazy=True))
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+
+#----------------------------------------------------------------------------#
+# Movies Model
+#----------------------------------------------------------------------------#
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    release = Column(DateTime)
+
+    def __init__(self, title, release):
+        self.title = title
+        self.release = release
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release': self.release
+        }
