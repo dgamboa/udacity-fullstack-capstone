73fbd0824a5dd237ed9f5137a5d86f13d80af405
Daniel Gamboa <danielgamboa10@gmail.com>
2020-11-14 14:22:45 -0600

    Add setup_db function to __init__ and import models

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -4,10 +4,12 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from models import setup_db, Actor, Movie, actor_movies
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    setup_db(app)
     CORS(app)
 
     return app
