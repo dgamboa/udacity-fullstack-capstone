4a1ccc4d0359d02b990c2955ec4d97b75274fbed
Daniel Gamboa <danielgamboa10@gmail.com>
2020-11-16 13:55:30 -0600

    Add environment variables with Auth0 domain, algo and audience into auth file

---
+++ backend/auth/auth.py
@@ -0,0 +1,22 @@
+import json
+import logging
+from flask import request, _request_ctx_stack, abort
+from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+
+
+AUTH0_DOMAIN = 'kane-films-capstone.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'resources'
+
+# Standard error messages for authentication
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+# Decorator method for the API to initiate authentication process
+def requires_auth(permission=''):
+    pass
